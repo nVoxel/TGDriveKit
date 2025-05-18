3073d81cdc4ecca60bcfd484aa913c677f521789
nVoxel <nvoxel@outlook.com>
2025-05-18 19:54:10 +0300

    Add files via upload

--- Package.swift
+++ Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         .binaryTarget(
             name: "TGDriveKit",
             url: "https://github.com/nVoxel/TGDriveKit/releases/download/0.1/TGDriveKit.xcframework.zip",
-            checksum:"fc378add95ab52faf00aed8a185391565daed83effeb26b8c3ce739d74db67c1")
-        ),
+            checksum:"fc378add95ab52faf00aed8a185391565daed83effeb26b8c3ce739d74db67c1"
+        )
     ]
 )
